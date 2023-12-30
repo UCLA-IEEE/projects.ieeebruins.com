8fc78cebee08e0455f1900091c7f6fc9ed3b1593
Paul Zhang <paulz@ucla.edu>
2023-12-29 16:15:25 -0800

    Add index page to welcome visitors (#7)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,11 +15,6 @@ const config = {
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: "UCLA-IEEE", // Usually your GitHub org/user name.
-  projectName: "projects.ieeebruins.com", // Usually your repo name.
-
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
 
